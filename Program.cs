a98d8ab6aa509f9a4e7a58aa9d89ca77cbdcb2a1
Igor <i.ergakoff@mail.ru>
2022-11-12 23:26:37 +0300

    Решение задания № 64

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: 
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(number, 1));
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + ", " + PrintNumbers(start - 1, end));
+}
+
+// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
+
+// Console.Write("Введите число M: ");
+// // int m = int.Parse(Console.ReadLine()!);
+// // Console.Write("Введите число N: ");
+// int n = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine(PrintNumbers(m, n));
