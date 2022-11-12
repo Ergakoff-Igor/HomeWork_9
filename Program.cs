3906531036813bf949e2f6017462c32ccceac9a6
Igor <i.ergakoff@mail.ru>
2022-11-12 23:48:44 +0300

    Решение задания № 66

--- Program.cs
+++ Program.cs
@@ -4,25 +4,31 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите число N: ");
-int number = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите число N: ");
+// int number = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine(PrintNumbers(number, 1));
+// Console.WriteLine(PrintNumbers(number, 1));
 
-string PrintNumbers(int start, int end)
-{
-    if (start == end) return start.ToString();
-    return (start + ", " + PrintNumbers(start - 1, end));
-}
+// string PrintNumbers(int start, int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + ", " + PrintNumbers(start - 1, end));
+// }
 
 // Задача 66: 
 // Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // // M = 4; N = 8. -> 30
 
-// Console.Write("Введите число M: ");
-// // int m = int.Parse(Console.ReadLine()!);
-// // Console.Write("Введите число N: ");
-// int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
 
-// Console.WriteLine(PrintNumbers(m, n));
+Console.WriteLine($"сумма чисел от {m} до {n} равна: {SumNumbers(m, n)}");
+
+int SumNumbers(int start, int end)
+{
+    if (start > end) return 0;
+    return start + SumNumbers(start + 1, end);
+}
